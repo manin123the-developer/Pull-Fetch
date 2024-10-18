073fdaf920cc4f6aa47c104ce69f2d3588553ed0
RAVI MANISANKAR REDDY <manisankarreddyravi3@gmail.com>
2024-10-18 15:39:51 +0530

    this is my first commit

---
+++ input.py
@@ -0,0 +1,5 @@
+
+name = input("Enter your name")
+print("Good morning!")
+print(f"You'r name is: {name+ 'How are you '}")
+
---
+++ sample.py
@@ -0,0 +1,15 @@
+a=10
+b=10
+print(a+b)
+print(a-b);
+print(a*b)
+c="Mani"
+print(f"the value of this operation is:{c}");
+print("Hello kavya ");
+
+
+list_1 = [10,20,30,40,50]
+#name=input("Enter your name");
+name="Enter your name"
+print(f"Hello {name}")
+      
